df646a0a1b228e949c11e59247e8b3f4cf52202e
呂駿宏 <148522235+party1168@users.noreply.github.com>
2026-04-06 23:39:31 +0800

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let cals = new Map();
+        for (let i = 0; i < nums.length; i++) {
+            let o = target - nums[i];
+            if (cals.has(nums[i])) return [cals.get(nums[i]), i];
+            cals.set(o, i);
+        }
+    }
+}
